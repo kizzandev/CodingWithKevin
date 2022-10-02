53dc94da909aa6b97d25bc78247becaecffed285
Kevin Zanzi <69643444+kevinzanzi@users.noreply.github.com>
2022-10-02 18:23:18 -0300

    added hackerrank problem solving challenges

---
+++ Python/HackerRank/DiagonalDifference/run.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+
+def diagonalDifference(arr):
+    # Write your code here
+    sum_a = 0
+    sum_b = 0
+
+    for i in range(len(arr)):
+        sum_a += arr[i][i]
+        sum_b += arr[i][len(arr)-1-i]
+
+    return abs(sum_a-sum_b)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
