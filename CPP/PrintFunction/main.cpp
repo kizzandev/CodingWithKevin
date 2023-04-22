d9437639c936cba376a2ffc83dec0c5bfe4407e5
Kevin Zanzi <69643444+kevinzanzi@users.noreply.github.com>
2023-04-22 03:04:29 -0300

    print and format functions
    Examples are provided

---
+++ CPP/PrintFunction/main.cpp
@@ -0,0 +1,26 @@
+#include "print.hpp"
+
+#include <string>
+
+class Test
+{
+public:
+    // This is called: conversion operator
+    operator const char *() const { return "Test"; };
+};
+
+int main()
+{
+    Test test;
+
+    print("Hello", " ", "World", "!"); // Hello World!
+    print("1 + 1 = ", 2);              // 1 + 1 = 2
+    print(test);                       // Test
+    print();                           // [empty line]
+
+    print(format("There was a {} in the {}.", "lion", "cave")); // There was a lion in the cave.
+    print(format("{} + {} = {}", "1", "2", 3));                 // 1 + 2 = 3
+
+    print(format("{} {}", 1, 2, 3)); // 1 2
+    print(format("{} {}", 1));       // 1 {}
+}
